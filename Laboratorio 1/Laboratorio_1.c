a57352116c8e19fc4499e0b37650625d1b5a9a49
Benjamin_Pavez <benjamin.pavez@usm.cl>
2023-08-05 21:59:02 -0400

    Add files via upload

---
+++ Laboratorio 1/Laboratorio_1.c
@@ -0,0 +1,123 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/stat.h>
+#include<dirent.h>
+#include<time.h>
+#include<string.h>
+
+
+
+/*
+La funcion a partir del nombre del archivo extrae la informacion necesaria
+
+Parametros :
+   char* nom_archivo : puntero tipo char que apunta al nombre del archivo a leer 
+   
+Retorno :
+   Nada, ya que es tipo void
+ 
+*/
+void Orientacion(char*nom_archivo){
+   printf("----------------------------------------------------------------\n");
+   printf("Nombre del Archivo: %s\n",nom_archivo);
+   printf("----------------------------------------------------------------\n");
+   FILE *archivo = fopen(nom_archivo, "r"); // Modo lectura
+   char bufer[1000];         // Aquí vamos a ir almacenando cada línea
+   while (fgets(bufer, 1000, archivo)){
+      if(bufer == "vertical"){
+         fgets(bufer, 1000, archivo);
+         int longitud = (strlen(bufer)-1);
+         printf("Orientacion: Vertical\n");
+         printf("Numero de Filas: %i\n",longitud);
+         printf("Numero de Columnas: %i\n",longitud);
+         printf("----------------------------------------------------------------\n");
+         printf("\n");
+         printf("\n");
+         break;
+      }else{
+         fgets(bufer, 1000, archivo);
+         int longitud = strlen(bufer);
+         printf("Orientacion: Horizontal\n");
+         printf("Numero de Filas: %i\n",longitud-1);
+         printf("Numero de Columnas: %i\n",longitud-1);
+         printf("----------------------------------------------------------------\n");
+         printf("\n");
+         printf("\n");
+         break;
+      }
+   }
+}
+
+
+
+/*
+La funcion revisa el directorio actual para encontrar los archivo txt a leer
+
+Parametros :
+   No recibe ningun parametro
+
+Retorno :
+   Nada, ya que es tipo void
+ 
+*/
+void LeerDir() {
+   DIR *d = opendir(".");
+   struct dirent *dentry;
+   size_t i = 1;
+   
+   while ((dentry = readdir(d)) != NULL) {
+      if (strstr(dentry->d_name, ".txt") != NULL) {
+         //printf("%lu. %s\n", i, dentry->d_name);
+         Orientacion(dentry->d_name);
+         i++;
+      }
+   }
+
+   closedir(d);
+}
+
+
+
+//Esta funcion quizas se borra para que no solo haga una funcion
+/*
+La funcion crear los directorios necesarios para guardar las sopas de letras
+
+Parametros :
+   int tipo : entero que representa si es de tipo horizontal o vertical
+
+Retorno :
+   Nada, ya que es tipo void
+ 
+*/
+void CrearDir(int tipo){
+   if(tipo == 0){
+      mkdir("./horizontal", S_IRWXU);
+      printf("Horizontal\n");
+   }else{
+      mkdir("./vertical", S_IRWXU);
+      printf("Vertical\n");
+   }
+
+}
+
+
+
+
+/*
+La funcion Main
+
+Parametros :
+   No recibe ningun parametro
+
+Retorno :
+   retorna 0 si se ejecuto correctamente
+ 
+*/
+int main(){
+   printf("Hola Mundo CTM\n");
+   //createDir(0);
+   LeerDir();
+   return 0;
+
+}
+
