f55ff96e10c1865cd0a00ebf4d440b48d383d0f9
Benjamin_Pavez <benjamin.pavez@usm.cl>
2023-09-12 22:30:52 -0300

    Update Magic_Maze.c

--- Laboratorio 2/Magic_Maze.c
+++ Laboratorio 2/Magic_Maze.c
@@ -7,6 +7,10 @@
 #include <dirent.h>
 #include <unistd.h>
 
+#define NUM_ACCIONES 6
+#define NUM_DIRECCIONES 4
+
+
 
 
 /*
@@ -19,7 +23,7 @@ Retorno :
    Retorna una estructura que almacena la matriz
  
 */
-char** Tablero(char* nom_archivo){
+char** CrearTablero(char* nom_archivo){
     //Leo el archivo para calcular la longitud de la linea
     FILE* archivo = fopen(nom_archivo, "r"); 
     char bufer[20];
@@ -37,7 +41,13 @@ char** Tablero(char* nom_archivo){
         matriz[i] = malloc(sizeof(char) * longitud); //Asigna memoria para cada fila
         for(int j = 0; j < longitud; j++) {
             fscanf(archivolectura, "%c", &matriz[i][j]);
-            printf("%c ",matriz[i][j]);
+            if(matriz[i][j] == 'J'){
+                printf("%c",matriz[i][j]);
+
+            }else{
+                printf(" %c ",matriz[i][j]);
+            }
+            
         }
         printf("\n");
     }
@@ -47,9 +57,83 @@ char** Tablero(char* nom_archivo){
 
 }
 
-int main(){
+int RandomPos(){
 
-    Tablero("archivo.txt");
+}
+
+
+void MostrarTablero(){
+
+    /*
+    ========================================
+               Magic Maze
+    ========================================
+
+            Jugador Principal       Bot 1
+            Tesoro: [ ]           Tesoro: [ ]
+
+    ----------------------------------------
+    | / | / | / | / | / | / | / | / | / | / |
+    ----------------------------------------
+    | / |   |   |   |   |   |   |   |   |   |
+    ----------------------------------------
+    | / |   |   |   |   |   |   |   |   |   |
+    ----------------------------------------
+    | / |   |   |   |   |   |   |   |   |   |
+    ----------------------------------------
+    | / |   |   |   |   |   |   |   |   |   |
+    ----------------------------------------
+
+            Jugador 3              Bot 2
+            Tesoro: [ ]           Tesoro: [ ]
+
+    ========================================
+    Turno Actual: Jugador Principal
+    ========================================
+    1. Acción: [Buscar] [Escaleras] [Derecha] [Izquierda] [Arriba] [Abajo]
+    Elige una acción: [ ]
+    */
+
+    printf("----------------------------------------");
+
+    }
+
+int main(){
+    printf("========================================\n");
+    printf("               Magic Maze               \n");
+    printf("========================================\n");
+    //printf("\n");
+
+    char *Acciones[NUM_ACCIONES] = {"Buscar", "Escaleras", "Derecha", "Izquierda", "Arriba", "Abajo"};
+    char *Direcciones[NUM_DIRECCIONES] = {"Norte", "Sur", "Este", "Oeste"}; 
+
+    char *Cartas[NUM_ACCIONES] = {"Buscar", "Escaleras", "Derecha", "Izquierda", "Arriba", "Abajo"};
+
+    char *cartas_disponibles[NUM_ACCIONES * 2 + NUM_DIRECCIONES * 6];
+    memcpy(cartas_disponibles, Acciones, sizeof(Acciones));
+    memcpy(cartas_disponibles + NUM_ACCIONES, Acciones, sizeof(Acciones));
+    memcpy(cartas_disponibles + 2 * NUM_ACCIONES, Direcciones, sizeof(Direcciones));
+    memcpy(cartas_disponibles + 2 * NUM_ACCIONES + NUM_DIRECCIONES, Direcciones, sizeof(Direcciones));
+
+
+    printf("Revolviendo cartas\n");
+    int val;
+    printf("Presione 1 para sacar las tus cartas: ");
+    scanf("%d", &val);
+    if(val == 1){
+        printf("Tus cartas son\n");
+        for(int i = 0; i<2; i++){
+            printf("%d. %s\n",i+1,cartas_disponibles[i]);
+        }
+        printf("\n");
+    }else{
+        exit(0);
+    }
+     
+    
+
+
+    CrearTablero("tablero1.txt");
 
     /*
     //Jugadores
